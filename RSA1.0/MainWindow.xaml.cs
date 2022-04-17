bd5cb39119bbf47379505a14119885f98cf72ba9
drDanik <62499116+danila-Skachedubov@users.noreply.github.com>
2022-04-17 22:37:31 +0300

    1.0

---
+++ RSA1.0/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RSA1._0
+{
+
+    public partial class MainWindow : Window
+    {
+        char[] characters = new char[] { '#', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
+                                                'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                'Э', 'Ю', 'Я', ' ', '1', '2', '3', '4', '5', '6', '7',
+                                                '8', '9', '0' };
+        List<string> value = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+        private void Shifr(object sender, EventArgs e)
+        {
+            if ((textBox_p.Text.Length > 0) && (textBox_q.Text.Length > 0))
+            {
+                long p = Convert.ToInt64(textBox_p.Text);
+                long q = Convert.ToInt64(textBox_q.Text);
+
+                if (IsTheNumberSimple(p) && IsTheNumberSimple(q))
+                {
+                    string s = "";
+                    s = Vvod.Text;
+                    s = s.ToUpper();
+
+                    long n = p * q;
+                    long m = (p - 1) * (q - 1);
+                    long d = Calculate_d(m);
+                    long e_ = Calculate_e(d, m);
+
+                    List<string> result = RSA_Endoce(s, e_, n);
+                    
+
+                    foreach (string item in result)
+                    {
+                        Output.Text += item.ToString()+"\n";
+                        value.Add(item);
+                    }
+
+                    textBox_d.Text = d.ToString();
+                    textBox_n.Text = n.ToString();
+
+                   
+                }
+                else
+                    MessageBox.Show("p или q - не простые числа!");
+            }
+            else
+                MessageBox.Show("Введите p и q!");
+        }
+        private void Rasshifr(object sender, EventArgs e)
+        {
+            if ((textBox_d.Text.Length > 0) && (textBox_n.Text.Length > 0))
+            {
+                long d = Convert.ToInt64(textBox_d.Text);
+                long n = Convert.ToInt64(textBox_n.Text);
+
+                List<string> input = new List<string>();
+
+                foreach (var item in value)
+                {
+                    input.Add(item.ToString());
+                }
+                
+
+              
+                
+                string result = RSA_Dedoce(input, d, n);
+                
+
+                Output_Copy.Text = result.ToLower(); 
+                
+
+                
+            }
+            else
+                MessageBox.Show("Введите секретный ключ!");
+        }
+        private bool IsTheNumberSimple(long n)
+        {
+            if (n < 2)
+                return false;
+
+            if (n == 2)
+                return true;
+
+            for (long i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+        private List<string> RSA_Endoce(string s, long e, long n)
+        {
+            List<string> result = new List<string>();
+
+            BigInteger bi;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                int index = Array.IndexOf(characters, s[i]);
+
+                bi = new BigInteger(index);
+                bi = BigInteger.Pow(bi, (int)e);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                result.Add(bi.ToString());
+            }
+
+            return result;
+        }
+        private string RSA_Dedoce(List<string> input, long d, long n)
+        {
+            string result = "";
+
+            BigInteger bi;
+
+            foreach (string item in input)
+            {
+                bi = new BigInteger(Convert.ToDouble(item));
+                bi = BigInteger.Pow(bi, (int)d);
+
+                BigInteger n_ = new BigInteger((int)n);
+
+                bi = bi % n_;
+
+                int index = Convert.ToInt32(bi.ToString());
+
+                result += characters[index].ToString();
+            }
+
+            return result;
+        }
+        private long Calculate_d(long m)
+        {
+            long d = m - 1;
+
+            for (long i = 2; i <= m; i++)
+                if ((m % i == 0) && (d % i == 0)) //если имеют общие делители
+                {
+                    d--;
+                    i = 1;
+                }
+
+            return d;
+        }
+        private long Calculate_e(long d, long m)
+        {
+            long e = 10;
+
+            while (true)
+            {
+                if ((e * d) % m == 1)
+                    break;
+                else
+                    e++;
+            }
+
+            return e;
+        }
+
+    }
+}
